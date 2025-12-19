3df837b54f2b9f07cd4b51d599f239af49947843
ariel0147 <arial055665@gmail.com>
2025-12-19 09:08:12 +0200

    משימות

--- routes/tasks_R.js
+++ routes/tasks_R.js
@@ -3,7 +3,17 @@ const router = express.Router();
 const {getAlltasks } = require("../controller/tasks_C");
 
 const {isLoggedIn} = require("../middelware/auth_MID");
+const {addcategories, getOneCategory, deleteCategory, updateCategory} = require("../controller/categories_C");
+const {isValidid} = require("../middelware/users_MID");
 
 router.get('/', isLoggedIn,getAlltasks);
+
+router.post('/',isLoggedIn,addtasks);
+
+router.get('/:id', isLoggedIn, isValidid, getOnetasks);
+
+router.delete('/:id', isLoggedIn, isValidid, deletetasks);
+
+router.patch('/:id', isLoggedIn, isValidid, updatetasks);
 
 module.exports = router;
