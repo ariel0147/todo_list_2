53a5796bf888b3a4d4bb90d559da9fbbca2a2d55
ariel0147 <arial055665@gmail.com>
2026-01-20 19:57:58 +0200

    עדכון לפניתי×בניית השלד דפי הhtml js והוספה לרוט

--- routes/pages_R.js
+++ routes/pages_R.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-
+const { isLoggedIn } = require("../middelware/auth_MID");
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "..", "public", "page", "login.html"));
 });
 
 
-router.get('/home', (req, res) => {
+router.get('/home', isLoggedIn, (req, res) => {
     res.sendFile(path.join(__dirname, "..", "public", "page", "index.html"));
 });
 
@@ -18,6 +18,15 @@ router.get('/reg', (req, res) => {
 
 router.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, "..", "public", "page", "login.html"));
+});
+
+
+router.get('/users_page', isLoggedIn, (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "page", "users.html"));
+});
+
+router.get('/categories_page', isLoggedIn, (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "page", "categories.html"));
 });
 
 module.exports = router;
